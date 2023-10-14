d358b141a4be466743d5c7af5691e64140d977dd
Tim Emmert <tim.emmert@tum.de>
2023-10-14 16:35:32 -0700

    Add make prompt from name

---
+++ backend/prompt_maker/make_prompt_from_name.py
@@ -0,0 +1,24 @@
+import os
+
+from backend.prompt_maker.make_prompt import conversation_buf
+
+
+def text_for_name(name: str):
+    path_of_this_python_folder = os.path.dirname(os.path.abspath(__file__))
+
+    path = os.path.join(path_of_this_python_folder, f"../../data/db/people/{name}.txt")
+    try:
+        with open(path, 'r') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return f"File not found: {path}"
+
+def prompt_from_name(name: str):
+    return conversation_buf(
+        text_for_name(name)
+    )
+
+print(
+    prompt_from_name("tim")
+)
