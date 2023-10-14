8338337e4a8a36b3ee0dd9aad2958381eb0e9bd7
Tim Emmert <tim.emmert@tum.de>
2023-10-14 16:54:15 -0700

    Add audio transcription

--- backend/audio-converter.py
+++ backend/audio-converter.py
@@ -6,10 +6,15 @@ os.environ['OPENAI_API_KEY'] = "sk-BHgwXBylLi0zS8xkUnSRT3BlbkFJyNRHwCbwXHODaNpuZ
 openai.api_key = os.environ['OPENAI_API_KEY']
 # Iterate over the data/db/audio directory
 # For each file, transcribe it and save the transcription to a file in the data/db/transcriptions directory
-files = os.listdir("/Users/tim/Documents/Sage AI Stanford/people-recommender/data/db/audio")
+path_of_this_python_folder = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path_of_this_python_folder, f"data/db/audio")
+
+files = os.listdir(path)
 for file in files:
-    audio_file = open(f"/Users/tim/Documents/Sage AI Stanford/people-recommender/data/db/audio/{file}", "rb")
+    name = file.split(".")[0]
+    audio_file = open(os.path.join(path, file), "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    with open(f"/Users/tim/Documents/Sage AI Stanford/people-recommender/data/db/transcriptions/{file}.txt", 'w') as file:
+    path = os.path.join(path_of_this_python_folder, f"../data/db/people/{name}.txt")
+    with open(path, 'w') as file:
         file.write(transcript)
     print(f"Transcribed {file}")
