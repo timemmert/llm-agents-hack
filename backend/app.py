6a9570affa97ba240dc125c96b947223c5a20543
Kaktus500 <delseith.paul@gmail.com>
2023-10-14 16:53:20 -0700

    Add route for user input

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+import os
+from pathlib import Path
+import json
+
+base_path = Path(__file__).parent
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/user_input", methods=["POST"])
+def store_text():
+    data = request.get_json()
+    name = data["name"]
+    with open(base_path / "data" / "db" / "people" / f"{name}.json", "a") as file:
+        json.dump(data, file)
+    return "Information stored successfully!"
