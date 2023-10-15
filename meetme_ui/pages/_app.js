09ca5cb8ac2235c07037c2193f4ba9ab50521820
Kaktus500 <delseith.paul@gmail.com>
2023-10-14 18:46:07 -0700

    Add basic convo result

--- meetme_ui/pages/_app.js
+++ meetme_ui/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import "../styles/chat.css";
+import "../styles/CompatibilityDisplay.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- meetme_ui/pages/compdisplay.js
+++ meetme_ui/pages/compdisplay.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import './CompatibilityDisplay.css';
+import { useEffect, useState } from 'react';
 
-function CompatibilityDisplay({ imageUrl, score, pros, cons, isLoading }) {
+function CompatibilityDisplay() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [convo_result, setConvoResult] = useState({score: 0, pros: [], cons: []});
+  useEffect(async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/run_convos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name: "test", message: userInput })
+    });
+
+      const data = await response.json()
+      setConvoResult(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [])
   if (isLoading) {
     return (
       <div className="compatibility-container">
@@ -13,12 +31,12 @@ function CompatibilityDisplay({ imageUrl, score, pros, cons, isLoading }) {
   return (
     <div className="compatibility-container">
       <img src={imageUrl} alt="Uploaded Image" className="image-display" />
-      <div className="score-display">Compatibility Score: {score}</div>
+      <div className="score-display">Compatibility Score: {convo_result.score}</div>
       <div className="list-container">
         <div className="pros">
           <h3>Pros</h3>
           <ul>
-            {pros.map((pro, index) => (
+            {convo_result.pros.map((pro, index) => (
               <li key={index}>{pro}</li>
             ))}
           </ul>
@@ -26,7 +44,7 @@ function CompatibilityDisplay({ imageUrl, score, pros, cons, isLoading }) {
         <div className="cons">
           <h3>Cons</h3>
           <ul>
-            {cons.map((con, index) => (
+            {convo_result.cons.map((con, index) => (
               <li key={index}>{con}</li>
             ))}
           </ul>
