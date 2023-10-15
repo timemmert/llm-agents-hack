97002874ab3a3dee2a0c5644ebd704d97cc83e9e
nraghuraman <nikhilvr@hotmail.com>
2023-10-14 17:44:11 -0700

    Actually hook it up

--- backend/matching_loop.py
+++ backend/matching_loop.py
@@ -2,6 +2,7 @@ import os
 
 from backend.agent_conversation import converse
 from backend.prompt_maker.make_prompt_from_name import prompt_from_name
+from evaluation.chain import compatibility_parser
 
 
 def do_conversations_for_human(human_name: str):
@@ -22,9 +23,15 @@ def do_conversations_for_human(human_name: str):
         prompt_conversation_partner = prompt_from_name(name)
         prompt_conversation_current_human = prompt_from_name(human_name)
 
-        messages = converse(prompt_conversation_partner['response'], prompt_conversation_current_human['response'])
+        conversation = converse(
+            prompt_conversation_current_human["response"],
+            prompt_conversation_partner["response"],
+        )
+
         # TODO: Plug matching results into evaluator
-        # conversation_partners_score = ...
+        conversation_partners_score = compatibility_parser(
+            prompt_conversation_current_human, conversation
+        )
     return conversation_partners_score
 
 
