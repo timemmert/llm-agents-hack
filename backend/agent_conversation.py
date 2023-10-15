bbe5b470f12a75fb10bf495e1772d885a70bf6d3
nraghuraman <nikhilvr@hotmail.com>
2023-10-14 17:32:04 -0700

    Improve quality of conversation

--- backend/agent_conversation.py
+++ backend/agent_conversation.py
@@ -13,15 +13,21 @@ def converse(person_1_description, person_2_description):
         temperature=0.9,
         openai_api_key="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",
     )
-    system_description = """ You are having a conversation with another person whom you are meeting for the first time.
-        You are not an AI, but rather an individual with the following description. Match the person in their tone of
-        voice. Be friendly, but not overly friendly.
+    # system_description = """ You are having a conversation with another person whom you are meeting for the first time.
+    #     You are not an AI, but rather an individual with the following description. Match the person in their tone of
+    #     voice. Be friendly, but not overly friendly.
+    #     {}
+    #     """
+    system_description = """ You are not an AI, but an individual with the following description. When
+        you converse, you must pretend as if you were this individual and mirror their personality, their hobbies,
+        their goals, etc. 
+        DESCRIPTION:
         {}
+        You are meeting another person for the first time. Have a casual conversation with them. Be friendly and
+        inquisitive, but not overly inquisitive. Do not regurgitate facts about yourself. Do not repeat yourself.
+        When you are ready to end the conversation, say "Goodbye"
         """
     template_rest = """
-        When conversing, please keep your conversation aligned with this description. Reference particular facts
-        about yourself from the description. However, do not regurgitate all facts, but rather reference them only
-        if that comes up in the conversation.
         Current conversation:
         {history}
         Human: {input}
@@ -77,15 +83,19 @@ def _converse(
             input=output_two,
         )
         print("1: ", output_one)
+        if "Goodbye" in output_one:
+            break
         output_two = conversation_two.predict(
             input=output_one,
         )
         print("2: ", output_two)
+        if "Goodbye" in output_two:
+            break
     messages = conversation_one.memory.buffer_as_messages
     return messages
 
 
-if "__name__" == __main__:
+if __name__ == "__main__":
     # person_1_description = "--------- Passionate about computer vision and animal rights----- BS/MS student at Stanford----- Enjoys reading, hiking, and playing video games----- Values honesty, integrity, and hard work----- Has a strong sense of justice and fairness----- Likes to explore new ideas and technologies----- Enjoys learning new things and meeting new people"
     person_1_description = "'\n1. Personality: Outgoing, adventurous, creative\n2. Values: Family, sports, books, music\n3. Background: 16-year-old from a small town in Indiana, from a big family, plays soccer, loves books, foodie, plays guitar\n4. Age: 16\n5. Name: Sarah\n6. Hobbies: Soccer, reading, cooking, playing guitar\n7. Goals: Balance sports, books, and baking\n8. Political Leanings: Unknown'"
     # person_2_description = (
